b8faa3f6d4f0964c44081e13258f96de70a948ea
lengli <allengli@yahoo.com.br>
2014-02-06 13:06:49 -0200

    Mutação DE
    Método estático. Preparando para outras implementações de DE

--- DECore/RotinaDE.cs
+++ DECore/RotinaDE.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using AlgoResult;
 using Functions;
 using System.Linq;
@@ -33,17 +34,73 @@ namespace DECore
         }
 
         //ui,j,G+1 = x1,j,G + F(x2,j,G − x3,j,G) + F(x4,j,G − x5,j,G) + ...
-        private double CalculoAtributo(List<IndividuoBin> selecao, int atr)
+        private static double CalculoAtributo(List<IndividuoBin> selecao, int atr, double _fatorF)
         {
             double atributo = selecao[0].Atributos[atr];
 
             for (int i = 1; i < selecao.Count; i++)
             {
-                int sinal = i%2 == 1 ? 1 : -1;
+                int sinal = i % 2 == 1 ? 1 : -1;
                 atributo += selecao[i].Atributos[atr] + _fatorF;
                 atributo *= sinal;
             }
             return atributo;
+        }
+
+        internal static void ExecutarMutacao(int atualInd, List<IndividuoBin> populacao, SelecaoDE _tipoSelecao, double _fatorF,
+            int _nAtributos, double _probCross, Bound _min, Bound _max, FuncValidarFronteira valFront, FuncAptidao aptidao)
+        {
+            Random rand = new Random(DateTime.Now.Millisecond);
+            List<IndividuoBin> selecao;
+            switch (_tipoSelecao)
+            {
+                case SelecaoDE.Rand1Bin: selecao = OperadoresDE.SelecaoAleatoria(3, populacao); break;
+                case SelecaoDE.Best1Bin: selecao = OperadoresDE.SelecaoBest(populacao); break;
+                case SelecaoDE.Rand2Bin: selecao = OperadoresDE.SelecaoAleatoria(5, populacao); break;
+                case SelecaoDE.RandToBest1Bin:
+                    selecao = OperadoresDE.SelecaoAleatoria(2, populacao);
+                    selecao.Insert(0, populacao[atualInd]);
+                    selecao.Insert(0, populacao.First());
+                    selecao.Insert(0, populacao[atualInd]);
+                    break;
+                case SelecaoDE.RandToBest2Bin:
+                    selecao = OperadoresDE.SelecaoAleatoria(4, populacao);
+                    selecao.Insert(0, populacao[atualInd]);
+                    selecao.Insert(0, populacao.First());
+                    selecao.Insert(0, populacao[atualInd]);
+                    break;
+                case SelecaoDE.CurrentToRand1Bin:
+                    selecao = OperadoresDE.SelecaoAleatoria(3, populacao);
+                    selecao.Insert(0, populacao[atualInd]);
+                    selecao.Insert(2, populacao[atualInd]);
+                    break;
+                default: return;
+            }
+
+            IndividuoBin individuoTemp = new IndividuoBin();
+
+            int jRand = rand.Next(0, _nAtributos - 1);
+            for (int j = 0; j < _nAtributos; j++)
+            {
+                if (rand.NextDouble() < _probCross || j == jRand)
+                {
+                    double atributo = CalculoAtributo(selecao, j, _fatorF);
+
+                    // tratamento de restrição
+                    if (atributo >= _min(j) && atributo <= _max(j)
+                        && (valFront == null || valFront(atributo, j)))
+                    {
+                        individuoTemp.Atributos.Add(atributo);
+                        continue;
+                    }
+                }
+
+                individuoTemp.Atributos.Add(populacao[atualInd].Atributos[j]);
+            }
+
+            individuoTemp.Aptidao = aptidao(individuoTemp.Atributos);
+            if (individuoTemp.Aptidao < populacao[atualInd].Aptidao)
+                populacao[atualInd] = individuoTemp;
         }
 
         //double _fatorFUsado;
@@ -54,58 +111,7 @@ namespace DECore
             //_fatorFUsado = _fatorF;
 
             for (int i = 0; i < populacao.Count; i++)
-            {
-                List<IndividuoBin> selecao;
-                switch (_tipoSelecao)
-                {
-                    case SelecaoDE.Rand1Bin: selecao = OperadoresDE.SelecaoAleatoria(3, populacao); break;
-                    case SelecaoDE.Best1Bin: selecao = OperadoresDE.SelecaoBest(populacao); break;
-                    case SelecaoDE.Rand2Bin: selecao = OperadoresDE.SelecaoAleatoria(5, populacao); break;
-                    case SelecaoDE.RandToBest1Bin:
-                        selecao = OperadoresDE.SelecaoAleatoria(2, populacao);
-                        selecao.Insert(0, populacao[i]);
-                        selecao.Insert(0, populacao.First());
-                        selecao.Insert(0, populacao[i]);
-                        break;
-                    case SelecaoDE.RandToBest2Bin:
-                        selecao = OperadoresDE.SelecaoAleatoria(4, populacao);
-                        selecao.Insert(0, populacao[i]);
-                        selecao.Insert(0, populacao.First());
-                        selecao.Insert(0, populacao[i]);
-                        break;
-                    case SelecaoDE.CurrentToRand1Bin:
-                        selecao = OperadoresDE.SelecaoAleatoria(3, populacao);
-                        selecao.Insert(0, populacao[i]);
-                        selecao.Insert(2, populacao[i]);
-                        break;
-                    default: return;
-                }
-
-                IndividuoBin individuoTemp = new IndividuoBin();
-
-                int jRand = rand.Next(0, _nAtributos - 1);
-                for (int j = 0; j < _nAtributos; j++)
-                {
-                    if (rand.NextDouble() < _probCross || j == jRand)
-                    {
-                        double atributo = CalculoAtributo(selecao, j);
-
-                        // tratamento de restrição
-                        if (atributo >= _min(j) && atributo <= _max(j)
-                            && (_validarFronteira == null || _validarFronteira(atributo, j)))
-                        {
-                            individuoTemp.Atributos.Add(atributo);
-                            continue;
-                        }
-                    }
-
-                    individuoTemp.Atributos.Add(populacao[i].Atributos[j]);
-                }
-
-                individuoTemp.Aptidao = FuncaoAptidao(individuoTemp.Atributos);
-                if (individuoTemp.Aptidao < populacao[i].Aptidao)
-                    populacao[i] = individuoTemp;
-            }
+                ExecutarMutacao(i, populacao, _tipoSelecao, _fatorF, _nAtributos, _probCross, _min, _max, _validarFronteira, FuncaoAptidao);
         }
 
         protected override bool CriterioDeParada(AlgoInfo agInfo)
