3542eb4c4eaa1207c90161b25bf47b31067bab50
lengli <allengli@yahoo.com.br>
2014-02-12 20:01:14 -0200

    Correções no SaDE
    - Current-to-rand
    - Equação geral do DE

--- DECore/DEUtil.cs
+++ DECore/DEUtil.cs
@@ -9,15 +9,14 @@ namespace DECore
     internal static class DEUtil
     {
         //ui,j,G+1 = x1,j,G + F(x2,j,G − x3,j,G) + F(x4,j,G − x5,j,G) + ...
-        internal static double CalculoAtributo(List<IndividuoBin> selecao, int atr, double _fatorF)
+        internal static double CalculoAtributo(List<IndividuoBin> selecao, int atr, List<double> fs)
         {
             double atributo = selecao[0].Atributos[atr];
-
             for (int i = 1; i < selecao.Count; i++)
             {
                 int sinal = i % 2 == 1 ? 1 : -1;
-                atributo += selecao[i].Atributos[atr] + _fatorF;
-                atributo *= sinal;
+                int j = (i - 1 - ((i - 1) % 2)) / 2;
+                atributo += (selecao[i].Atributos[atr] * fs[j]) * sinal;
             }
             return atributo;
         }
@@ -28,27 +27,37 @@ namespace DECore
         {
             Random rand = new Random(DateTime.Now.Millisecond);
             List<IndividuoBin> selecao;
+            List<double> fs;
+
             switch (_tipoSelecao)
             {
-                case SelecaoDE.Rand1Bin: selecao = OperadoresDE.SelecaoAleatoria(3, populacao); break;
-                case SelecaoDE.Best1Bin: selecao = OperadoresDE.SelecaoBest(populacao); break;
-                case SelecaoDE.Rand2Bin: selecao = OperadoresDE.SelecaoAleatoria(5, populacao); break;
+                case SelecaoDE.Rand1Bin:
+                    selecao = OperadoresDE.SelecaoAleatoria(3, populacao);
+                    fs = new List<double> { _fatorF };
+                    break;
+                case SelecaoDE.Best1Bin: selecao = OperadoresDE.SelecaoBest(populacao);
+                    fs = new List<double> { _fatorF }; break;
+                case SelecaoDE.Rand2Bin: selecao = OperadoresDE.SelecaoAleatoria(5, populacao);
+                    fs = new List<double> { _fatorF, _fatorF }; break;
                 case SelecaoDE.RandToBest1Bin:
                     selecao = OperadoresDE.SelecaoAleatoria(2, populacao);
                     selecao.Insert(0, populacao[atualInd]);
                     selecao.Insert(0, populacao.First());
                     selecao.Insert(0, populacao[atualInd]);
+                    fs = new List<double> { _fatorF, _fatorF };
                     break;
                 case SelecaoDE.RandToBest2Bin:
                     selecao = OperadoresDE.SelecaoAleatoria(4, populacao);
                     selecao.Insert(0, populacao[atualInd]);
                     selecao.Insert(0, populacao.First());
                     selecao.Insert(0, populacao[atualInd]);
+                    fs = new List<double> { _fatorF, _fatorF, _fatorF };
                     break;
                 case SelecaoDE.CurrentToRand1Bin:
                     selecao = OperadoresDE.SelecaoAleatoria(3, populacao);
                     selecao.Insert(0, populacao[atualInd]);
                     selecao.Insert(2, populacao[atualInd]);
+                    fs = new List<double> { rand.NextDouble(), _fatorF };
                     break;
                 default: return;
             }
@@ -60,7 +69,7 @@ namespace DECore
             {
                 if (rand.NextDouble() < _probCross || j == jRand)
                 {
-                    double atributo = CalculoAtributo(selecao, j, _fatorF);
+                    double atributo = CalculoAtributo(selecao, j, fs);
 
                     // tratamento de restrição
                     if (atributo >= _min(j) && atributo <= _max(j)
