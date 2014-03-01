ca57f51ec11054164d97ea2e404b6558a4f16439
lengli <allengli@yahoo.com.br>
2014-03-01 15:11:05 -0300

    Estratégia RandomDE

---
+++ DECore/RotinaRandomDE.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AlgoCore;
+using Functions;
+using AlgoResult;
+
+namespace DECore
+{
+    public class RotinaRandomDE : RotinaAlgo
+    {
+        public RotinaRandomDE(FuncAptidao ap, FuncRepopRestricao rp, ListAptidao gs, ListAptidao hs, FuncValidarRestricao vl, FuncValidarFronteira fr)
+            : base(ap, rp, gs, hs, vl, fr) { }
+
+        protected override bool CriterioDeParada(AlgoResult.AlgoInfo agInfo) { return false; }
+
+        protected override void InicializarAlgoritmo(List<AlgoResult.IndividuoBin> populacao) { }
+
+        public override void ExecutarAlgoritmo(List<AlgoResult.IndividuoBin> populacao)
+        {
+            for (int i = 0; i < populacao.Count; i++)
+            {
+                IndividuoBin individuo = null;
+                if (new Random(AlgoUtil.GetSeed()).NextDouble() > 0.0)
+                    individuo = IndividuoBin.GerarPopulacao<IndividuoBin>(1, _min, _max,
+                        _nAtributos, IndividuoBin.Precisao, null, 0).FirstOrDefault();
+
+                DEUtil.ExecutarMutacao(i, populacao, (SelecaoDE)new Random(AlgoUtil.GetSeed()).Next(0, 5),
+                    new Random(AlgoUtil.GetSeed()).NextDouble() * 0.9 + 0.1,
+                    _nAtributos, new Random(AlgoUtil.GetSeed()).NextDouble() * 0.6 + 0.3,
+                    _min, _max, _validarFronteira, FuncaoAptidao, null, null,
+                    //new Random(AlgoUtil.GetSeed()).Next(0, 100));
+                    0);
+            }
+        }
+    }
+}
--- DECore/SeleceaoDE.cs
+++ DECore/SeleceaoDE.cs
@@ -2,12 +2,12 @@
 {
     public enum SelecaoDE
     {
-        Rand1Bin,
-        Rand2Bin,
-        Best1Bin,
-        RandToBest1Bin,
-        RandToBest2Bin,
-        CurrentToRand1Bin,
-        CurrentToPBest1BinArchive
+        Rand1Bin = 0,
+        Rand2Bin = 1,
+        Best1Bin = 2,
+        RandToBest1Bin = 3,
+        RandToBest2Bin = 4,
+        CurrentToRand1Bin = 5,
+        CurrentToPBest1BinArchive = 6
     }
 }
