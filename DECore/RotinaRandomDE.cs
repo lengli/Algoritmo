6d4843950239983cc9426783e3cce278e3ea9527
lengli <allengli@yahoo.com.br>
2014-03-10 16:37:52 -0300

    Correções
    - threshold utilizava o parametro errado para o n. da ger. atual
    - Geração de um indivíduo aleatório depois de n falhas, não estava
    realmente utilizando o novo individuo

--- DECore/RotinaRandomDE.cs
+++ DECore/RotinaRandomDE.cs
@@ -13,7 +13,7 @@ namespace DECore
         private double _cr, _f, _alfa, _gama;
         private int _ger;
 
-        public RotinaRandomDE(double cr, double f, double alfa,double gama, int ger,
+        public RotinaRandomDE(double cr, double f, double alfa, double gama, int ger,
             FuncAptidao ap, FuncRepopRestricao rp, ListAptidao gs, ListAptidao hs, FuncValidarRestricao vl, FuncValidarFronteira fr)
             : base(ap, rp, gs, hs, vl, fr)
         {
@@ -33,7 +33,7 @@ namespace DECore
         private double _margem;
         public override void ExecutarAlgoritmo(List<IndividuoBin> populacao)
         {
-            _margem = _alfa * _nAtributos * Math.Pow((_geracoesMAx - _ger) / _geracoesMAx, _gama);
+            _margem = _alfa * _nAtributos * Math.Pow((double)(_geracoesMAx - g) / _geracoesMAx, _gama);
             if (_ger != 0 && _gerFalhas >= _ger)
             {
                 IndividuoBin individuo = IndividuoBin.GerarPopulacao<IndividuoBin>(1, _min, _max,
@@ -42,13 +42,13 @@ namespace DECore
                 _gerFalhas = 0;
                 _melhorAptAnterior = double.MaxValue;
 
-                List<IndividuoBin> popAux = populacao.Concat(new List<IndividuoBin> { individuo }).ToList();
+                populacao.Insert(0, individuo);
 
-                DEUtil.ExecutarMutacao(0, popAux,
+                DEUtil.ExecutarMutacao(0, populacao,
                     (SelecaoDE)new Random(AlgoUtil.GetSeed()).Next(0, 5),
-                    _f == 0 ? new Random(AlgoUtil.GetSeed()).NextDouble() * 0.9 + 0.1 : _f,
+                    _f <= 0 ? new Random(AlgoUtil.GetSeed()).NextDouble() * 0.9 + 0.1 : _f,
                     _nAtributos,
-                    _cr == 0 ? new Random(AlgoUtil.GetSeed()).NextDouble() * 0.6 + 0.3 : _cr,
+                    _cr <= 0 ? new Random(AlgoUtil.GetSeed()).NextDouble() * 0.6 + 0.3 : _cr,
                     _min, _max, _validarFronteira, FuncaoAptidao, null, null, _margem);
             }
             else
@@ -56,16 +56,16 @@ namespace DECore
                 {
                     DEUtil.ExecutarMutacao(i, populacao,
                         (SelecaoDE)new Random(AlgoUtil.GetSeed()).Next(0, 5),
-                        _f == 0 ? new Random(AlgoUtil.GetSeed()).NextDouble() * 0.9 + 0.1 : _f,
+                        _f <= 0 ? new Random(AlgoUtil.GetSeed()).NextDouble() * 0.9 + 0.1 : _f,
                         _nAtributos,
-                        _cr == 0 ? new Random(AlgoUtil.GetSeed()).NextDouble() * 0.6 + 0.3 : _cr,
+                        _cr <= 0 ? new Random(AlgoUtil.GetSeed()).NextDouble() * 0.6 + 0.3 : _cr,
                         _min, _max, _validarFronteira, FuncaoAptidao, null, null, _margem);
                 }
 
             if (_ger == 0) return;
 
             double melhorApt = populacao.Min(x => x.Aptidao);
-            
+
             if (melhorApt < _melhorAptAnterior)
             {
                 _melhorAptAnterior = melhorApt;
